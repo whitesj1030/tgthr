715747a35235ad9624e112c02c952b252077a6b2
whitesj1030 <35138219+whitesj1030@users.noreply.github.com>
2020-07-30 00:46:14 +0900

    first commit

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -0,0 +1,36 @@
+package com.thoughtmechanix.licenses.clients;
+
+import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.utils.UserContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * OAuth2RestTemplate로 OAuth2 엑세스 토큰전파
+ * @author white
+ *
+ */
+@Component
+public class OrganizationRestTemplateClient {
+    @Autowired
+    RestTemplate restTemplate;
+
+    private static final Logger logger = LoggerFactory.getLogger(OrganizationRestTemplateClient.class);
+
+    public Organization getOrganization(String organizationId){
+        logger.debug("In Licensing Service.getOrganization: {}", UserContext.getCorrelationId());
+
+        ResponseEntity<Organization> restExchange =
+                restTemplate.exchange(
+                        "http://zuulserver:5555/api/organization/v1/organizations/{organizationId}",
+                        HttpMethod.GET,
+                        null, Organization.class, organizationId);
+
+        return restExchange.getBody();
+    }
+}
